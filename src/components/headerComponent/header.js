bf7c07fea4bb9e90afffe9565516b7aa43213a3d
Jane Scott <53611363+tu-amo@users.noreply.github.com>
2021-04-19 22:34:02 +0200

    initial
    frame

---
+++ src/components/headerComponent/header.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+
+       Test Header
+
+       </header>
+    );
+  }
+}
+// function Header() {
+//  return (
+//    <header className="App">
+//    test my header
+//    </header>
+//  );
+//}
+
+export default Header;
